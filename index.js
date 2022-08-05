1f15d7ec91eb4a450d9f7f2213d9b83b2b87544e
Rahul Patil <patil-0-01997@gmail.com>
2022-08-05 18:40:31 +0530

    First  js Project

---
+++ index.js
@@ -0,0 +1,227 @@
+/*console.log("Hello World..... ");
+console.warn("This is warning ..");
+console.error("this is an error");
+*/
+//1. Variable
+//what is variables:- Container to store the data values;
+
+/*
+var numbr=50;
+var numbr2=60;
+console.log(numbr+numbr2);
+*/
+//2. Data Types
+
+//Number 
+/*
+var number1=500;
+var number2=1000;
+
+
+console.log(number1);
+console.log(number2);
+*/
+
+// String 
+
+/*
+var str1 ="This is String ";
+var str2 ='This is also a String ';
+
+console.log(str1);
+console.log(str2);
+
+*/
+
+// Objects
+/*
+var marks={
+    English:65,
+    Hindi:92,
+    Maths:75,
+    Physics:88,
+    chemistry:80}
+
+console.log(marks)
+
+*/
+//Boolean 
+/*
+var a=true;
+var b=false;
+
+console.log(a);
+console.log();
+
+*/
+//undefine
+/*
+var und;
+console.log(und);
+
+*/
+// Sysmbol
+/*
+Symbol('Rahul');
+console.log(Symbol);
+
+*/
+/*
+At a very high level,there are two typs 1.Primitive and 2. Reference
+
+*/
+
+
+//Array 
+
+/*
+var arr=[1,2,3,4,5];
+ 
+console.log(arr);
+
+*/
+
+
+
+// Arthmetic Operators
+
+/*var a=100;
+var b=10;
+
+console.log(" The value of A + B  is = "+(a+b));
+console.log(" The value of A - B  is = "+(a-b));
+console.log(" The value of A * B  is = "+a*b);
+console.log(" The value of A / B  is = "+a/b);
+*/
+
+//logical  AND operators
+
+/*
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+// logical OR opertors
+
+console.log(true || true);
+console.log(true || true);
+console.log(true || true);
+console.log(false || false);
+
+*/
+
+
+// Function :- function is a block of code designed to parform a particular task. 
+
+/*
+function voting( age ){
+
+    if(age>=18){
+        console.log("You are eligble for Voting ");
+    }else{
+
+        console.log("You are not eligble for Voting ");
+    }
+}
+
+var c=voting(45);
+
+*/
+
+// conditional  injava scripts
+
+//loops 
+
+//var arr=[2,4,6,8,10,12,14,16,18,20];
+
+//console.log(arr);
+
+//for(var i=0;i<arr.length;i++){
+  //  console.log(arr[i]);
+//}
+/*
+arr.forEach(function(element){
+    console.log(element);
+})
+
+// while
+
+let j=0;
+while(j<arr.length){
+    console.log(arr[j]);
+    j++;
+}
+
+
+console.log("...............");
+// do while 
+
+do{
+    console.log(arr[j]);
+j++;
+}while(j<arr.length);
+
+*/
+
+/// Array methods
+
+//let arr=[1,"Rahul",3,4,"Neha",6,7,"Bunny",9,10];
+
+
+//console.log(arr.length);
+
+//arr.push("Hiral");
+
+//console.log(arr); 
+
+
+
+/// String methods 
+
+
+//let mystring ="Welcome to the India ";
+
+//console.log(mystring.length);
+
+
+//console.log(mystring.replace("India","Rahul"));
+
+
+// Js date 
+
+/*
+let mydate = new Date();
+console.log(mydate);
+
+console.log(mydate.getFullYear());
+console.log(mydate.getDay());
+console.log(mydate.getMonth());
+
+*/
+
+//Arroww Function 
+
+
+//sum=(a,b)=>{
+  //  return a+b;
+//}
+//console.log(sum(4,5));
+
+
+//settimeout and setintervel
+
+//hello=()=>{
+  //  console.log("Hello Rahul.......");
+//}
+
+//clr=setInterval(hello,2000);
+//clearInterval(clr);
+
+
+
+
+
+
+
+
